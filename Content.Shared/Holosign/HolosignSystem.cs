88838dbd2e8795337736e5c866c6f540aafd77b0
TriviaSolari <154280615+TriviaSolari@users.noreply.github.com>
2026-02-26 16:08:58 -0500

    No need to snap to grid

--- Content.Shared/Holosign/HolosignSystem.cs
+++ Content.Shared/Holosign/HolosignSystem.cs
@@ -1,9 +1,7 @@
-using Content.Shared.Coordinates.Helpers;
 using Content.Shared.Examine;
 using Content.Shared.Interaction;
 using Content.Shared.PowerCell;
 using Content.Shared.Storage;
-using Robust.Shared.Map.Components;
 using Robust.Shared.Network;
 
 namespace Content.Shared.Holosign;
@@ -11,7 +9,6 @@ namespace Content.Shared.Holosign;
 public sealed class HolosignSystem : EntitySystem
 {
     [Dependency] private readonly PowerCellSystem _powerCell = default!;
-    [Dependency] private readonly SharedTransformSystem _transform = default!;
     [Dependency] private readonly INetManager _net = default!;
 
     public override void Initialize()
@@ -52,16 +49,7 @@ public sealed class HolosignSystem : EntitySystem
         // overlapping of the same holo on one tile remains allowed to allow holofan refreshes
         if (ent.Comp.PredictedSpawn || _net.IsServer)
         {
-            var coordinates = args.ClickLocation;
-
-            // Snap to grid coordinates if one exists
-            var gridUid = _transform.GetGrid(coordinates);
-            if (TryComp<MapGridComponent>(gridUid, out var grid))
-            {
-                coordinates = coordinates.SnapToGrid(grid);
-            }
-
-            var holosign = PredictedSpawnAtPosition(ent.Comp.SignProto, coordinates);
+            var holosign = PredictedSpawnAtPosition(ent.Comp.SignProto, args.ClickLocation);
             Transform(holosign).LocalRotation = Angle.Zero;
         }
 
